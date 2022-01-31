2cf28e10e6ca21a2a5c2aa1bc98428a22ca688ca
Vikour <vikour92@uma.es>
2022-01-31 02:52:18 -0600

    Commit inicial

---
+++ src/test/java/es/vikour/nss/nssreservahoteles/NssReservaHotelesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vikour.nss.nssreservahoteles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssReservaHotelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
