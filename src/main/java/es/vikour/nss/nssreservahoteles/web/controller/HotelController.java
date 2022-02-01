986f9e5bdf8c756075a2beb24eff9d69bd82a9ca
Vikour <vikour92@uma.es>
2022-02-01 03:43:16 -0600

    Documentación de la funcionalidad 'Listar hoteles'

--- src/main/java/es/vikour/nss/nssreservahoteles/web/controller/HotelController.java
+++ src/main/java/es/vikour/nss/nssreservahoteles/web/controller/HotelController.java
@@ -3,6 +3,7 @@ package es.vikour.nss.nssreservahoteles.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,10 @@ import es.vikour.nss.nssreservahoteles.entity.Hotel;
 import es.vikour.nss.nssreservahoteles.service.HotelService;
 import es.vikour.nss.nssreservahoteles.web.converter.HotelToDtoConverter;
 import es.vikour.nss.nssreservahoteles.web.dto.HotelDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/hotels")
@@ -23,7 +28,12 @@ public class HotelController {
 	@Autowired
 	private HotelToDtoConverter hotelDtoConverter;
 	
-	@GetMapping
+	@Operation(
+		summary = "Lista los hoteles",
+		description = "Lista los hoteles que hay en el sistema")
+	@ApiResponses(
+		@ApiResponse(responseCode = "200", description = "Listado correctamente, se devuelve la lista de hoteles"))
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<HotelDto>> findAllHotels() {
 		List<Hotel> hotelList = hotelService.findAll();
 		List<HotelDto> hotelDtoList = hotelDtoConverter.hotelToDto(hotelList);
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO hotels(name, category) VALUES ('Hotel Córdoba Center', 4);
+INSERT INTO hotels(name, category) VALUES ('Eurostars Palace', 4);
+INSERT INTO hotels(name, category) VALUES ('Soho Boutique Córdoba', 4);
+INSERT INTO hotels(name, category) VALUES ('Ítaca Colón', 3);
